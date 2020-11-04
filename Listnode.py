9806350d8b2628b284b7a9fef91844dd8382f0d7
tjlujianqiao <tjlujianqiao@gmail.com>
2020-11-04 21:47:18 +0800

    '20201104'

--- Listnode.py
+++ Listnode.py
@@ -21,6 +21,7 @@ class Ownlist(object):
         return None
     def findnode(self, nodevalue, nprevious, listnode):
         for i in range(nprevious):
+
             listnode = listnode.pre
             if listnode.pre != None:
                 if listnode.value == nodevalue:
@@ -28,7 +29,6 @@ class Ownlist(object):
         return None
     def insertbefore(self, listnode, nodevalue):
         if listnode != None:
-
             node = Listnode(nodevalue, listnode.pre, listnode)
             listnode.pre.next = node
             listnode.pre =  node
@@ -37,17 +37,20 @@ class Ownlist(object):
             self.header.next = node
             self.trailer.pre = node
     def delete(self, listnode):
-        value = listnode.value
-        listnode.pre.next = listnode.next
-        listnode.next.pre = listnode.pre
+        if listnode.pre != None:
+         listnode.pre.next = listnode.next
+        if listnode.next != None:
+         listnode.next.pre = listnode.pre
     def duplicate(self, listnode):
-        r = 0
+
+        pre = 0
         while listnode.next != None:
-           find_same_node = self.findnode(listnode.value, r, listnode)
+           find_same_node = self.findnode(listnode.value, pre, listnode)
            if find_same_node!= None:
                self.delete(find_same_node)
+               pre =  pre - 1 
            else:
-               r = r + 1
+               pre = pre + 1
                listnode = listnode.next
         return 
     def printallnodes(self,listnode):
@@ -75,13 +78,39 @@ class Ownlist(object):
             last = last.next
             length = length + 1
         length = length -1
-        import pdb
-        pdb.set_trace()
+
         while (length > 0 ):
-            max_node = self.selectmin(first, length)
-            self.insertbefore(last,max_node.value)
-            self.delete(max_node)     
+            min_node = self.selectmin(first, length)
+            self.insertbefore(last,min_node.value)
+            self.delete(min_node)     
             length = length - 1   
+        # selectsort 会保证没有排序的元素至少不会大于已经排序了的元素， 类似于bubblersort, 但是select的效率远远高于bubblersort, 每次都要比较和交换
+        #每次都是相邻元素间的移动排序
+    def findnodeforinsert(self, nodevalue,listnode):
+        p = listnode
+        while p.pre.value != None:
+            if p.value >= nodevalue:
+                p = p.pre
+            else:
+                return p.next
+
+        return None
+    def insert_sort(self, listnode):
+
+        cur = listnode
+        while cur.next != None:   
+            nodevalue = cur.value
+            insert_location = self.findnodeforinsert(nodevalue, cur)
+            if insert_location != None:
+                self.insertbefore(insert_location, nodevalue)
+                cur = cur.next
+                if cur != None:
+                 self.delete(cur)
+            else:
+                cur = cur.next
+        
+        
+
 
 
 
@@ -97,15 +126,23 @@ def copynodes(listnode, n_numbers):
            return ownlist
 if __name__ == "__main__":
     ownlist = Ownlist()
+    ownlist1 = Ownlist()
     ownlist.insertbefore(None,1)
     ownlist.insertbefore(ownlist.header.next,2)
     ownlist.insertbefore(ownlist.header.next,2)
     ownlist.insertbefore(ownlist.header.next,3)
     ownlist.printallnodes(ownlist.header.next)
+    ownlist1.insertbefore(None,1)
+    ownlist1.insertbefore(ownlist1.header.next,2)
 
-    ownlist.duplicate(ownlist.header.next)
+    ownlist1.insertbefore(ownlist1.header.next,3)
+    ownlist.duplicate(ownlist.header.next) 
     print('fdasfd')
     ownlist.printallnodes(ownlist.header.next)
     ownlist.select_sort(ownlist.header.next)
     print("aoligei")
     ownlist.printallnodes(ownlist.header.next)
+    print("aoligei2")
+    ownlist1.printallnodes(ownlist1.header.next)
+    ownlist1.insert_sort(ownlist1.header.next)
+    ownlist1.printallnodes(ownlist1.header.next)
