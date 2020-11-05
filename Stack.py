9a879ac4419b9d707dfe55cbfba9c9235ca267af
tjlujianqiao <tjlujianqiao@gmail.com>
2020-11-05 12:15:47 +0800

    '1215'

--- Stack.py
+++ Stack.py
@@ -39,7 +39,7 @@ def jinzhizhuanhuan(jinzhiweishu, originalvalue):
          cur_value = multipers
     return stack
 def columncomp(string):
-    stack = Stack([0])
+    stack = Stack([])
     for item in string:
         if item == '(' or "[" or '{' :
             stack.push(item)
@@ -55,8 +55,30 @@ def columncomp(string):
             continue
     return True
 # 栈可以用于实时匹配过程，这一性质是减治和分治都不可做到的
+# 栈混洗还挺有意思的奥， 栈混洗中，原位置的三个顺序元素不能出现 j+1, i, j for j >i, 这里用>表示距离栈顶的位置，当然这个算法需要枚举任意三个元素的相对位置
+# 所以我们还是采用栈模拟的方式来印证
+def judgestack(orginalstack, mixedstack):
+
+    stack_b = Stack([])
+    stack_a = Stack(orginalstack)
+    stack_c = Stack(mixedstack)
+    while len(stack_a.list) > 0:
+      if stack_c.top() in stack_b.list: 
+        if stack_b.top() == stack_c.top():
+            stack_b.pop()
+            stack_c.pop()
+        else:
+            return False
+      stack_b.push(stack_a.pop())
+    if len(stack_b.list) == 0:
+        return True
+    else:
+        return False
+
 
 if __name__ == "__main__":
    jinzhizhuanhuan(2,10).print_all()
    print(columncomp('[111]'))
+   print(judgestack('321', '213'))
+
     
