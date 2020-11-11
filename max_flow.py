18427476cc681920bb8fd2f755487dfbea90502a
tjlujianqiao <tjlujianqiao@gmail.com>
2020-11-11 11:51:20 +0800

    'max-flow'

--- max_flow.py
+++ max_flow.py
@@ -1,44 +1,49 @@
-class Edge(object):
-        def __init__(self, nodename ):
-           self.nodename= nodename
+
 class Vertex(object):
         def __init__(self, vertexname):
             self.name =vertexname
             self.parent = None
-all_name =[]
+
+Vertex_list = [Vertex('0'), Vertex('1'),Vertex('2'),Vertex('3'),Vertex('4'),Vertex('5')]
 class Graphic(object):
         def __init__(self):
-              self.graphdict= {}
+            self.graphdict= {}
         def AddEdge(self, first_vertex, second_vertex, edgecapacity):
-              self.graphdict[(first_vertex,second_vertex)] = edgecapacity
+            self.graphdict[(first_vertex,second_vertex)] = edgecapacity
         def findmaxflow(self):
-
-            if self.findonepath():
+            total_value = 0
+            while self.findonepath():
+                 flow_path = self.findonepath()
                  return_vertex = self.findonepath()
-                 flow_value = 0
+                 flow_value = 1000
                  while (return_vertex.parent!=None):
-
-                     flow_value = max(flow_value, self.graphdict[(return_vertex.parent.name,return_vertex.name)])
-
-                     print(return_vertex.name)
+                     flow_value = min(flow_value, self.graphdict[(return_vertex.parent.name,return_vertex.name)])
                      return_vertex = return_vertex.parent
-                 print(return_vertex.name)
+                 self.updategraphdict(flow_path,flow_value)
+                 total_value = total_value + flow_value
+            print(total_value)
+        def updategraphdict(self,flow_path,flow_value):
+            while(flow_path.parent != None ):
+                 self.graphdict[(flow_path.parent.name,flow_path.name)]  =  self.graphdict[(flow_path.parent.name,flow_path.name)] - flow_value
+                 self.graphdict[(flow_path.name, flow_path.parent.name)] =  flow_value
+                 flow_path = flow_path.parent
         def findonepath(self):
              initial_vertex = '0'
              end_vertex =  '5'
              visited_edge ={}
              flowpath = {}
-             Vertex_list = [Vertex('0'),Vertex('1'),Vertex('2'),Vertex('3'),Vertex('4'),Vertex('5')]
              checking_list = [Vertex_list[0]]
              while (len(checking_list)!=0):
                  starting_vertex = checking_list.pop(0)
+                 visited_edge[starting_vertex.name] = True
                  for vertex in Vertex_list:
-                     if (starting_vertex.name,vertex.name) in  self.graphdict and (vertex.name) not in visited_edge:
-                         checking_list.append(vertex)
-                         visited_edge[vertex.name] =True
-                         vertex.parent = starting_vertex
-                         if vertex.name == end_vertex:
-                           return Vertex_list[-1]
+                     if (starting_vertex.name,vertex.name) in  self.graphdict and vertex.name not in visited_edge:
+                          if self.graphdict[(starting_vertex.name,vertex.name)] > 0:
+                            checking_list.append(vertex)
+                            visited_edge[vertex.name] =True
+                            vertex.parent = starting_vertex
+                            if vertex.name == end_vertex:
+                              return Vertex_list[-1]
              return False
 if __name__ == "__main__":
       g = Graphic()
