218b6db4c94064567bca4a864642e6ca3318b25e
tjlujianqiao <tjlujianqiao@gmail.com>
2020-11-24 12:00:44 +0800

    'ceshi'

--- AVL_tree.py
+++ AVL_tree.py
@@ -1,7 +1,7 @@
 from bintree import binnode
 
 class Binst(binnode):
-
+     
     def search(self, nodevalue):
       current_node = self
       previous_node = self
@@ -64,15 +64,47 @@ class Binst(binnode):
                 new_node = self.find_leftmostchild(deleted_node.rchild).value
                 deleted_node.value =new_node.value
                 self.change_parent(new_node,new_node.rchild)
-        
-                
-            
                 
             return 'finished'
         else:
             new_node = self.find_leftmostchild(deleted_node.rchild)
             
             return 'there exist no node to be deleted'
+    #首先你要搞清楚为啥会失衡，因为插入和删除操作确实导致节点高度的变化从个人导致整个avl数不再满足左右高度最多只相差1，但是由于每次插入和删除操作都是基于叶子节点才来的，所以
+    #如果是插入，插在none-type上面一旦发生了失衡也一定不是发生在父亲节点，而是祖父节点。。这就是为啥如果发生了失衡，一定会存在祖孙三代节点的原因。
+    #然后3+4的算法实在解释说，如果我想让树结构是重新平衡一定会要找一个左右平衡的，同时还要满足中序遍历，所以就有了3+4 （祖孙三代提出来重新拍成父子结构，然后4棵子树为了满足中序遍历结果重新排序）
+    def three_plus_four(self,nodes, subtrees):
+      for i in range(len(nodes) -1):
+        for j in range(len(nodes) - i):
+            if nodes[j].value > nodes[j+1].value:
+                midd = nodes[j+1]
+                nodes[j+1] = nodes[j]
+                nodes[j] = midd
+      for i in range(len(subtrees) -1):
+        for j in range(len(subtrees) - i):
+           
+            if nodes[j].value > nodes[j+1].value:
+                midd = nodes[j+1]
+                nodes[j+1] = nodes[j]
+                nodes[j] = midd     
+      nodes[1].lchild = nodes[0]
+      nodes[1].rchild = nodes[2]
+      nodes[0].rchild = subtrees[0]
+      nodes[0].lchild = subtrees[1]
+      nodes[1].rchild = subtrees[2]
+      nodes[1].rchild = subtrees[3]
+    
+def avlbalanced(binnode):
+    if -2 < status(binnode.lchild) - status(binnode.rchild) <2 :
+        return True
+    else:
+        return False
+
+def status(binnode):
+    if binnode:
+        return binnode.height
+    else:
+        return -1
 if __name__ == "__main__":
     binnode = Binst(3) 
     binnode.middletraverse()
@@ -80,3 +112,4 @@ if __name__ == "__main__":
     binnode.middletraverse()
     binnode.remove(2)
     binnode.middletraverse()
+    print(status(binnode))
